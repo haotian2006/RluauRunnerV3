e080c001ba985043400dca8fee549f06422f212a
Haotian2006 <haotian2090@gmail.com>
2026-08-25 21:00:24 -0700

    add cors headers

--- src/http/server.js
+++ src/http/server.js
@@ -8,6 +8,14 @@ const { registerWebRoutes } = require("../web");
 
 const app = express();
 
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  if (req.method === "OPTIONS") return res.sendStatus(204);
+  next();
+});
+
 app.use(express.json({ limit: BODY_LIMIT }));
 
 registerRespondRoutes(app);
